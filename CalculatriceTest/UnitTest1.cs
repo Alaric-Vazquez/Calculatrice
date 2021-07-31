cd8cd5da45e11c5956cc7423d9849bed40f62b31
Alaric-Vazquez <vazquez.alaric@gmail.com>
2021-07-31 15:57:50 +0200

    Creat Unit Test project

---
+++ CalculatriceTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatriceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
